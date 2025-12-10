a36de7bbeac362fd96bae615ccea86e1791ec16a
N8N-Como <ventas@comoencasa.info>
2025-12-10 14:02:21 +0100

    Add files via upload

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -25,7 +25,7 @@ export const formatDuration = (milliseconds: number): string => {
 export const getDistanceFromLatLonInMeters = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
   const R = 6371e3; // Radius of the Earth in meters
   const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - 1);
+  const dLon = deg2rad(lon2 - lon1); // CORREGIDO: Antes ponía (lon2 - 1)
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
